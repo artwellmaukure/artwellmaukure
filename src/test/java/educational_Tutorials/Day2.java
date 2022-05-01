b4e4dbf5ab9bb0934c8a085347c142f8640ae1b1
artwellmaukure <artwellmaukure@gmail.com>
2022-05-01 12:08:59 +0200

    Adding element locators

---
+++ src/test/java/educational_Tutorials/Day2.java
@@ -0,0 +1,35 @@
+package educational_Tutorials;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Day2 {
+    WebDriver driver;
+    //JavascriptExecutor jse;
+
+    public void invokeBrowser(){
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\Ina Gous\\IdeaProjects\\web_driver\\resources\\chromedriver.exe");
+        driver =new ChromeDriver();
+        driver.manage().deleteAllCookies();
+        driver.manage().window().maximize();
+
+        driver.get("https://www.google.com");
+
+
+        WebElement name =driver.findElement(By.name("q"));
+        name.sendKeys("life meaning");
+
+        WebElement arts=driver.findElement(By.name("btnK"));
+        arts.click();
+
+        ;[]
+    }
+
+    public static void main(String args[]){
+        Day1 mybj = new Day1();
+        mybj.invokeBrowser();
+    }
+
+}
